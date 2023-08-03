b333cec4499d28c2d33541accf38e532fc442dc4
Sascha Wilking <Sascha.Wilking@hmmh.de>
2023-08-03 11:02:25 +0200

    initial commit with dashboard widgets

---
+++ Configuration/Backend/DashboardPresets.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'solr_file_indexer_admin' => [
+        'title' => 'LLL:EXT:solr_file_indexer_admin/Resources/Private/Language/locallang_db.xlf:dashboard.solr_file_indexer',
+        'description' => 'LLL:EXT:solr_file_indexer_admin/Resources/Private/Language/locallang_db.xlf:dashboard.solr_file_indexer.description',
+        'iconIdentifier' => 'content-dashboard',
+        'defaultWidgets' => ['sfiIndexableDocumentsDomain', 'sfiIndexableDocumentsLanguage', 'sfiIndexedDocuments'],
+        'showInWizard' => true
+    ],
+];
