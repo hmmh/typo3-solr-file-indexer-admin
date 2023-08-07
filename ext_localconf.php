dd36370895f630421abb4938f9bcf7e41ed6459c
Sascha Wilking <Sascha.Wilking@hmmh.de>
2023-08-07 12:04:52 +0200

    add missing template files

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,11 @@
 <?php
+
 if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+ExtensionManagementUtility::addTypoScriptSetup(
+    'module.tx_dashboard.view.templateRootPaths.20 = EXT:solr_file_indexer_admin/Resources/Private/Templates'
+);
